4266fabcda85a31458cc814ff1749e9cf81871b0
YuriZalukovskiy <55449630+YuriZalukovskiy@users.noreply.github.com>
2019-10-18 02:31:15 +0300

    Залуковский Юрий АИ-183
    Практика №2
    1) Quicksort - самая быстрая
    2) Heapsort - более медленная
    3) Mergesort - самая долгая

--- first.py
+++ first.py
@@ -1,30 +1,115 @@
-from random import randrange
-import time
 from datetime import datetime
 
-#buble
-my_list = [ randrange(0, 15) for i in range(10) ]
+def mergeSort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+  
+        mergeSort(left)
+        mergeSort(right)
+  
+        i = j = k = 0
 
-max_list = len( my_list )
+        while i < len(left) and j < len(right): 
+            if left[i] < right[j]: 
+                array[k] = left[i] 
+                i += 1
+            else: 
+                array[k] = right[j] 
+                j += 1
 
-i = 0
-while i < max_list:
+            k+=1
 
-    j = 0
-    while j < max_list-i-1:
+        while i < len(left): 
+            array[k] = left[i] 
+            i += 1
+            k += 1
+          
+        while j < len(right): 
+            array[k] = right[j] 
+            j += 1
+            k += 1
 
-        if my_list[ j ] > my_list[ j + 1 ]:
+def heapify(array, length, i): 
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-            my_list[ j ], my_list[ j + 1] = my_list[ j + 1], my_list[ j ]
-        j+=1
-    i += 1
+    if left < length and array[i] < array[left]: 
+        largest = left
 
-print( my_list )
+    if right < length and array[largest] < array[right]: 
+        largest = right
+  
+    if largest != i: 
+        array[i], array[largest] = array[largest], array[i]
+  
+        heapify(array, length, largest) 
 
+def heapSort(array): 
+    length = len(array) 
 
+    for i in range(length, -1, -1): 
+        heapify(array, length, i) 
 
-from datetime import datetime
+    for i in range(length - 1, 0, -1): 
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0) 
+
+def partition(array, low, high):
+    i = (low - 1)
+    pivot = array[high]
+  
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1 
+            array[i], array[j] = array[j], array[i] 
+  
+    array[i + 1], array[high] = array[high], array[i + 1] 
+
+    return (i + 1)
+
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+    
+    return array
+
+f = open('file.txt', 'r')
+
+originalArray = []
+
+while True:
+    if f.read(1) == '6':
+        if f.read(2) == ': ':
+            symbol = f.read(1)
+
+            while symbol != '}':
+                originalArray.append(int(symbol))
+                symbol = f.read(1)
+            
+            break
+
+f.close()
+
+array = originalArray.copy()
 start_time = datetime.now()
-# do your work here
+mergeSort(array)
 end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
+print('Duration: {}'.format(end_time - start_time)) # Duration: 0:00:00.000085
+
+array = originalArray.copy()
+start_time = datetime.now()
+heapSort(array)
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time)) # Duration: 0:00:00.000070
+
+array = originalArray.copy()
+start_time = datetime.now()
+quickSort(array, 0, len(array) - 1)
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time)) # Duration: 0:00:00.000044
